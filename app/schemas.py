88f8f4c93d226168e95988b04ac87ed4cc8e004c
June Somsanith <j.somsanith08@gmail.com>
2025-12-13 13:22:08 -0500

    Testing

--- app/schemas.py
+++ app/schemas.py
@@ -5,7 +5,7 @@ from typing import List, Optional
 class StudyMetadata(BaseModel):
     source_id: str = Field(..., description="The ID from Genelab or other exteranal source (e.g., GLDS-123)")
     title: str
-    organism: List[str]
+    organism: List[str] = Field(default_factory = list)
     factors: List[str] = Field(default=[], description="Experimental factors (e.g., Spaceflight, Radiation, Gravitational factor, etc.)")
     description: Optional[str] = None
 
