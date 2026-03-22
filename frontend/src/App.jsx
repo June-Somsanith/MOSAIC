0590885cf20d1b8b1b4276d3f9c03e105d59d13d
June Somsanith <j.somsanith08@gmail.com>
2026-03-22 12:33:52 -0400

    Adding API Recruitment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,10 +18,45 @@ export default function App() {
     if (cleanId && !studies.includes(cleanId)) {
       setStudies([...studies, cleanId]);
       setInputValue('');
+      }
     }
-  }
-};
+  };
 
-const handRemoveStudy = (idToRemove) => {
-  
-}
+  const handRemoveStudy = (idToRemove) => {
+    setStudies(studies.filter(id => id !== idToRemove));
+  };
+
+  // --- WARNING MODAL ---
+  const initiateAnalysis = () => {
+    if (studies.length === 0) {
+      setError("Please add at least one NASA OSDR study ID to begin.");
+      return;
+    }
+    setError(null);
+
+    // Check:
+    if (studies.length > 5) {
+      setShowWarningModal(true);
+    } else {
+      executeMetaAnalysis();
+    }
+  };
+
+  // --- API RECRUITMENT ---
+  const executeMetaAnalysis = async () => {
+    setShowWarningModal (false);
+    setIsProcessing (true);
+    setError(null);
+
+    try {
+      // Recruiting the Backend Engine
+      const response = await fetch('http://127.0.0.1:8000/analyze/meta', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ study_ids: studies })
+      });
+    
+    if (!response.ok) {
+      throw new Error('Failed to execute meta-analysis');
+    }
+    }
